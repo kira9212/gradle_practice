2845a84c2ee3593bdcc46358c9f265f480750fcb
kira9212 <kira9212@naver.com>
2021-04-18 14:31:51 +0900

    3rd

--- src/main/java/com/sb/board/controller/BoardController.java
+++ src/main/java/com/sb/board/controller/BoardController.java
@@ -1,4 +1,21 @@
 package com.sb.board.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BoardController {
+
+
+    @GetMapping("/Index.do")
+    public String Index(Model model) {
+
+
+        model.addAttribute("username", "수한");
+        return "Index";
+    }
+
+
 }
